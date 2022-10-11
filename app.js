f705fcdd4e037de6d0f7c67fb893e7b9a262bb37
Benkalio <officialtennysonbk@gmail.com>
2022-10-11 10:59:12 +0100

    delete operation

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const pgp = require('pg-promise')()
 const connectionString = 'postgres://localhost:5432/posttest'
 const db = pgp(connectionString)
 
+// DELETE RECORD FROM DB 
+db.none('DELETE FROM tech WHERE techid = $1', [5])
+  .then((data) => {
+    console.log(data);
+  }).catch((error) => console.log(error))
+
+// UPDATE DB 
+// db.none('UPDATE tech SET price = $1, imageurl = $2 WHERE techid = $3', [4500,'Cloud',6])
+//   .then(() => {
+//     console.log("DONE");
+//   }).catch((error) => console.log(error));
+
 // Retrieve TABLE 
-db.any('SELECT name,course,price,imageURL FROM tech WHERE price < $1;',[4000])
-  .then((tech) => {
-    console.log(tech);
-  }).catch((error) => console.log(error));
+// db.any('SELECT name,course,price,imageURL FROM tech WHERE price < $1;',[4000])
+//   .then((tech) => {
+//     console.log(tech);
+//   }).catch((error) => console.log(error));
 
 
 // ADD 
