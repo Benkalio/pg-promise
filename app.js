4befa90dcefe243c174a2c279c049d241140e78f
Benkalio <officialtennysonbk@gmail.com>
2022-10-11 10:34:36 +0100

    Retrieving data from db

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const pgp = require('pg-promise')()
 const connectionString = 'postgres://localhost:5432/posttest'
 const db = pgp(connectionString)
 
-db.none('INSERT INTO tech(name,course,price,imageURL) VALUES($2,$4,$3,$1)', ['Fullstack', 'Developer', 3400, 'fullstack.png'])
-  .then(() => {
-    console.log('Success');
-  }).catch((error) => console.log(error))
+// Retrieve TABLE 
+db.any('SELECT name,course,price,imageURL FROM tech WHERE price < $1;',[4000])
+  .then((tech) => {
+    console.log(tech);
+  }).catch((error) => console.log(error));
+
+
+// ADD 
+// db.none('INSERT INTO tech(name,course,price,imageURL) VALUES($2,$4,$3,$1)', ['Fullstack', 'Developer', 3400, 'fullstack.png'])
+//   .then(() => {
+//     console.log('Success');
+//   }).catch((error) => console.log(error))
+
+// INSERT ONE (CREATE)
+// db.one('INSERT INTO tech(name,course,price,imageURL) VALUES($2,$4,$3,$1) RETURNING techid',['Cloud', 'Engineer', 4200, 'cloudengineer.png'])
+//   .then((data) => {
+//     console.log(data.techid);
+//   }).catch((error) => console.log(error))
